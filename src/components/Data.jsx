a7473730bc42f44f73e54302f774fc3c477dbb30
Aaqib Lone <aictehelpdesk4@gmail.com>
2023-11-02 14:46:54 +0530

    first react

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -3,54 +3,103 @@ import '../css/data.css'
 import logo from '../images/sbil.png'
 import document from '../images/document.png'
 import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+// import ChildComponent from './ChildComponent';
 
 function Data() {
+  const location = useLocation();
+  const { receivedImage, p_file } = location.state || {};
   const [imageUrl, setImageUrl] = useState('');
+  const [QRData, setQRData] = useState('');
   const endpoint="get_image"
 
   const dataFetch=()=>{
     axios.get(`http://localhost:5000/${endpoint}`)
-      .then((response) => {
-        setImageUrl(response.data);
-        console.log(response.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching image URL:', error);
-      });
+    .then((response) => {
+      setImageUrl(response.data);
+      console.log(response.data)
+      // console.log("Your satus is "+response.status)
+    })
+    .catch((error) => {
+      console.error('Error fetching image URL:', error);
+    });
   }
+  const QrData=()=>{
+    axios.get(`http://localhost:5000/qr_data`)
+    .then((response) => {
+      setQRData(response.data);
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('Error fetching image URL:', error);
+    });
+  }
+  useEffect(() => {
+    dataFetch()
+    QrData()
+    // Make an axios request to fetch the image data and update `imageUrl`
+    // Use `receivedImage` prop to display the image
+  }, [receivedImage]);
     // Fetch image URL from the API
 
-  return (
-    <>
-    <div className='data_div'>
-    <img className='data_logo' src={logo} alt='image'/>
-    </div>
-    <div className='info'>
-            <h4 className='info_tag'>Document Type: Aadhar Card </h4>
-            <h4 className='info_tag'>Document Last 4 digits: {imageUrl.aadhaar_last_4_digit} </h4>
+    // console.log(p_file)
+    return (
+      <>
+      {/* {imageUrl.Document_type === 'Aadhaar' && ( */}
+        {/* <> */}
+        <div className='data_div'>
+          <img className='data_logo' src={logo} alt='image' />
+        </div>
+        <div className='info'>
+          <h4 className='info_tag'>Document Type: {imageUrl.Document_type} </h4>
+          {imageUrl.Document_type=='PAN CARD' && <h4 className='info_tag'>Document View: FRONT </h4>}
+          {imageUrl.Document_type!='PAN CARD' && <h4 className='info_tag'>Document View: {imageUrl.view} </h4>}
+            {imageUrl.Document_type=='Aadhaar' && <h4 className='info_tag'>Document Last 4 digits: {QRData.aadhaar_last_4_digit} </h4>}
+            {imageUrl.Document_type=='Aadhaar' && <h4 className='info_tag'>Name: {QRData.name} </h4>}
+            {imageUrl.Document_type=='DRIVING LICENSE' && <h4 className='info_tag'>State: {imageUrl.State} </h4>}
+            {/* {imageUrl.Document_type=='PAN CARD' && <h4 className='info_tag'>Father's Name: {imageUrl.Father_Name} </h4>} */}
+            {imageUrl.Document_type!='Aadhaar' &&<h4 className='info_tag'>Name: {imageUrl.name} </h4>}
+            {imageUrl.Document_type=='Aadhaar' && <h4 className='info_tag'>Gender: {imageUrl.gender} </h4>}
+            <h4 className='info_tag'>DOB: {imageUrl.dob} </h4>
+            {/* <h4 className='info_tag'>Address: {imageUrl.house
+} {imageUrl.street} {imageUrl.subdistrict} {imageUrl.state}</h4> */}
+            <h4 className='info_tag'>Doc Number: {imageUrl.DocNo} </h4>
+        </div>
+        <div className='data'>
+        <div className='data_left'>
+            <h1>Before</h1>
+            <img className='document_img' src={p_file} alt='image' onClick={dataFetch}/>
+            <h3>Size of the image: 8mb</h3>
+          </div>
+          {imageUrl.Document_type=='Aadhaar' && <> <div className='data_right'>
+            <h1>After</h1>
+            <img className='document_img' src={`data:image/jpeg;base64,${receivedImage}`} alt='Received Image' />
+            <h3>Size of the image: 4mb</h3>
+          </div></>}
+        </div>
+        {/* </> */}
+        {imageUrl.Document_type=='Aadhaar' && <div style={{padding:'20px', marginLeft:'20px'}}>
+            <h2>QR Code Data</h2>
+            <h4>{JSON.stringify(QRData)}</h4>
+          </div>}
+        )
+
+{/* {imageUrl.Document_type === 'Pan_Card' && (
+        <>
+        <div className='data_div'>
+          <img className='data_logo' src={logo} alt='image' />
+        </div>
+        <div className='info'>
+          <h4 className='info_tag'>Document Type: {imageUrl.Document_type} </h4>
             <h4 className='info_tag'>Name: {imageUrl.name} </h4>
             <h4 className='info_tag'>Gender: {imageUrl.gender} </h4>
             <h4 className='info_tag'>DOB: {imageUrl.dob} </h4>
-            <h4 className='info_tag'>Address: {imageUrl.house
-} {imageUrl.street} {imageUrl.subdistrict} {imageUrl.state}</h4>
-    </div>
-    <div className='data'>
-        <div className='data_left'>
-            <h1>Before</h1>
-            {/* <p> All the data before processing</p> */}
-            <img className='document_img' onClick={dataFetch} src={imageUrl} alt='image'/>
-            <h3>Size of the image: 8mb</h3>
+            <h4 className='info_tag'>Address: {imageUrl.address}</h4>
+            <h4 className='info_tag'>Doc Number: {imageUrl.DocNo} </h4>
         </div>
-        <div className='data_right'>
-            <h1>After</h1>
-            {/* <p> All the data after processing</p> */}
-            <img className='document_img' src={document} alt='image'/>
-            <h3>Size of the image: 4mb</h3>
-           
-        </div>
-    </div>
-    </>
-  )
+        </>
+        )} */}
+</>
+    );
 }
-
 export default Data
--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,77 +1,82 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import '../css/file.css'
-import sbil from '../images/sbil.png'
-import { Link } from 'react-router-dom';
-function File() {
-const [selectedFile, setSelectedFile] = useState(null);
-// const [selectedOption, setSelectedOption] = useState(null);
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../css/file.css';
+import logo from '../images/sbil.png';
+import sbil from '../images/sbil.png';
+import { Link, useNavigate } from 'react-router-dom';
 
-// const handleOption= async(e)=>{
-//   setSelectedOption(e.target.value);
-//   // console.log(e.target.value)
-//   try {
-//     await axios.post("http://127.0.0.1:5000/upload")
-    
-//   } catch (error) {
-//     console.log(error)
-    
-//   }
+function File(props) {
+  const [selectedFile, setSelectedFile] = useState('');
+  const [formData, setFormData] = useState({
+    selectedFile: null,
+    message: '',
+  });
+  const [receivedImage, setReceivedImage] = useState(null);
+  const [preview_file, setPreview_file] = useState(null);
 
-
-// }
-
-const handleFileChange = (e) => {
-  setSelectedFile(e.target.files[0]);
-};
-
-const handleUpload = async () => {
-  const data = new FormData();
-  data.append('image', selectedFile);
-
-  try {
-    await axios.post("http://127.0.0.1:5000/upload", data)
-      .then((response) => {
-        console.log(response.data.message);
-        setSelectedFile(null);
-      });
-  } catch (error) {
-    console.error(error);
-  }
-  // Rest of your code...
-
-  // selectedFile contains the selected file
-  if (selectedFile) {
-    // Perform the file upload using an API or other methods
-    console.log('Selected file');
-  } else {
-    alert('Please select a file first.');
-  }
-};
-
-return (
-  <>
-  <div className='body'>
-    <img className='logo' src={sbil}/>
-    <div className='option'>
-      <h2 className='select-type'>Select file type:   </h2>
-      <select  name="select" id="select" className='select' onChange={handleUpload}>
-          <option id="aadhar"  value="aadhar_card">Aadhar Card</option>
-          <option id='pan' value="pan_card">Pan Card</option>
-          <option id='dl' value="driving_licence">Driving Licence</option>
-          <option id='voter' value="voter_card">Voter ID</option>
-          <option id='passport' value="passport">Passport</option>
-        </select>
-    </div>
-    <div>
-    <input className='upload-input' type="file" onChange={handleFileChange} />
-    <Link to='/data'><button className='upload-btn' onClick={handleUpload}>Upload</button></Link>
-    </div>
-  </div>
-  </>
-);
-
+  const navigate = useNavigate(); // Initialize the navigation object
   
+  const handleChangeOption = (e) => {
+    setFormData({
+      ...formData,
+      message: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+
+  const handleFileChange = (e) => {
+    setFormData({
+      ...formData,
+      selectedFile: e.target.files[0]
+    });
+    // setSelectedFile(e.target.files[0]);
+    setPreview_file(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const data = new FormData();
+    data.append('image', formData.selectedFile);
+    data.append('message', formData.message);
+
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/upload", data);
+      console.log(response.data.message);
+      console.log("Your status is "+response.status);
+      setReceivedImage(response.data.processed_image);
+
+      navigate('/data', { state: { receivedImage: response.data.processed_image, p_file: URL.createObjectURL(preview_file) } }); // Use navigate to pass data
+    } catch (error) {
+      console.error(error);
+      setReceivedImage(error)
+    }
+  };
+  console.log(formData.message)
+  return (
+    <>
+      <div className='body'>
+        <img className='logo' src={sbil} alt="Logo" />
+        <div className='option'>
+          <h2 className='select-type'>Select file type: </h2>
+          <select name="select" id="select" className='select' onChange={handleChangeOption}>
+          <option value="">Select an option</option>
+            <option id="aadhar" value="aadhar_card">Aadhar Card</option>
+            <option id='pan' value="pan_card">Pan Card</option>
+            <option id='dl' value="driving_licence">Driving Licence</option>
+            <option id='voter' value="voter_card">Voter ID</option>
+            <option id='passport' value="passport">Passport</option>
+          </select>
+        </div>
+        <div>
+          <input className='upload-input' type="file" onChange={handleFileChange} />
+          <Link to='/data'>
+            <button className='upload-btn' onClick={handleUpload}>Upload</button>
+            {/* <button className='upload-btn'>Submit</button> */}
+          </Link>
+        </div>
+      </div>
+      {/* <div>{receivedImage}</div> */}
+    </>
+  );
 }
 
-export default File
+export default File;
